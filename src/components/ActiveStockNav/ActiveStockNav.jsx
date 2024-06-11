4e85148737ca4402d1d455d2cd89678989f1dc22
ramachandrapetla <ramachandra1996@gmail.com>
2024-06-11 08:41:39 -0500

    Added back arrow

--- src/components/ActiveStockNav/ActiveStockNav.jsx
+++ src/components/ActiveStockNav/ActiveStockNav.jsx
@@ -1,9 +1,10 @@
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import './ActiveStockNav.css'
 
 const ActiveStockNav = () => {
     return (
         <div className="active-stock-nav pl-10">
-            <div className="back-to-markets">-- Markets </div>
+            <div className="back-to-markets"><ArrowBackIcon /> <div>Markets</div></div>
             <div className="stock-heading">$AAPL</div>
         </div>
     )
